267f14321d6f9895d86fb992fe1653b3c6bb4f3f
Guy <guy.moore.jr@gmail.com>
2018-11-06 14:11:44 -0700

    Updating some files

---
+++ model/userinfo.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const userInfoSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isAdmin:{
+        type: Boolean,
+        default: false
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    fName: {
+        type: String,
+    },
+    lName: {
+        type: String,
+    },
+    age:{
+        type: Number,
+    },
+    filename:{
+        type: String
+    },
+    gender:{
+        type: String,
+    },
+    programmingLanguages: [{
+        type: String,
+    }],
+    like:{
+        type: Boolean,
+    },
+    dislike:{
+        type: Boolean
+    },
+    experience:{
+        type: String,
+    },
+    spokenLanguage:[{
+        type: String
+    }],
+    teacher:{
+        type: String,
+
+    },
+    student:{
+        type: String,
+
+    }
+});
+
+module.exports = mongoose.model('UserInfo',userInfoSchema);
---
+++ routes/userinfo.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const {Router} = express;
+const multer = require('multer');
+const path = require ('path');
+
+// Routes
+
+const UserInfo = require('../model/userinfo');
+const userInfoRouter = Router();
+
+const upload = multer({
+    dest: './temp',
+    limits: {fileSize: 100000}
+});
+
+userInfoRouter.route('/')
+    .get((req, res, next) =>{
+        UserInfo.find({},req.query, (err, foundUserInfo) =>{
+            if(err){
+                res.status(400)
+                next(err)
+            }
+            else res.status(200).send(foundUserInfo)
+        })
+    })
+    .post(upload.single('file'),(req,res,next)=>{
+        req.body.filename = req.body.filename;
+        const newUserInfo = new UserInfo(req.body);
+        newUserInfo.save((err, savedUserInfo) =>{
+            if(err){
+                res.status(400)
+                next(err)
+            }
+            else res.status(201).send(savedUserInfo)
+        })
+    })
+userInfoRouter.route('/:id')
+    .get((res,req,next) =>{
+        UserInfo.findById(req.params.id, (err, foundUserInfo) =>{
+            if(err){
+                res.status(400)
+                next(err)
+            }
+            else res.status(200).send(foundUserInfo)
+        })
+    })
+    .delete((req,res) =>{
+        UserInfo.findByIdAndDelete(req.params.id, (err, deleteUserinfo) =>{
+            if(err){
+                res.status(400)
+                next(err)
+            } else{
+                res.status(204).send(deleteUserinfo)
+            }
+        })
+    })
+    .put((req,res) =>{
+        UserInfo.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, editedUserInfo) =>{
+            if(err){
+                res.status(400)
+                next(err)
+            }else{
+                res.status(200).send(editedUserInfo)
+            }
+        })
+    })
+UserInfo.route("/img/:filename")
+    .get((req,res,next) =>{
+        res.sendFile(path.resolve(__dirname, `../temp/${req.params.filename}`))
+    })
+
+module.exports = userInfoRouter;
